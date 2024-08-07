77d8ee617e05e7a22720267b705a6b18dbd0bcdc
Danil Treskov <treskovdanilv@gmail.com>
2024-08-07 17:16:31 +0300

    Add UserCreate view, add route for register to urls.

--- bike_rental/rentals/urls.py
+++ bike_rental/rentals/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from bike_rental.rentals.views import BikeViewSet, RentalViewSet
+from bike_rental.users.views import UserCreate
 
 app_name = 'rentals'
 
@@ -10,5 +11,6 @@ router_v1.register(r'bikes', BikeViewSet, 'bikes')
 router_v1.register(r'rentals', RentalViewSet, 'rentals')
 
 urlpatterns = [
+    path('register/', UserCreate.as_view(), name='register'),
     path('', include(router_v1.urls)),
 ]
