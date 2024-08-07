65199ebbb11237eef20d842a5947bbea4edb8dd5
Danil Treskov <treskovdanilv@gmail.com>
2024-08-07 17:24:39 +0300

    Add routes for Tokens.

--- bike_rental/rentals/urls.py
+++ bike_rental/rentals/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from bike_rental.rentals.views import BikeViewSet, RentalViewSet
 from bike_rental.users.views import UserCreate
@@ -11,6 +13,8 @@ router_v1.register(r'bikes', BikeViewSet, 'bikes')
 router_v1.register(r'rentals', RentalViewSet, 'rentals')
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', UserCreate.as_view(), name='register'),
     path('', include(router_v1.urls)),
 ]
