d0bbcecfd0031fae800f8a1fabac1285eb46455f
Danil Treskov <treskovdanilv@gmail.com>
2024-08-07 16:18:06 +0300

    Create Bike model.

--- bike_rental/rentals/models.py
+++ bike_rental/rentals/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+class Bike(models.Model):
+    """
+    Модель для представления велосипеда.
+    """
+
+    name = models.CharField(max_length=100)
+    is_available = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Велосипед'
+        verbose_name_plural = 'Велосипеды'
+
+    def __str__(self):
+        return self.name
+
