f453d12740ad4e3309b77f3fbdf910a25028a8d0
Danil Treskov <treskovdanilv@gmail.com>
2024-08-07 17:34:59 +0300

    Add end_rental method to RentalViewSet.

--- bike_rental/rentals/views.py
+++ bike_rental/rentals/views.py
@@ -1,3 +1,6 @@
+from django.utils import timezone
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Bike, Rental
@@ -12,6 +15,13 @@ class BikeViewSet(ModelViewSet):
 class RentalViewSet(ModelViewSet):
     queryset = Rental.objects.all()
     serializer_class = RentalSerializer
+
+    @action(detail=True, methods=['post'])
+    def end_rental(self, request, pk=None):
+        rental = self.get_object()
+        rental.end_time = timezone.now()
+        rental.save()
+        return Response({'status': 'rental ended.'})
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
