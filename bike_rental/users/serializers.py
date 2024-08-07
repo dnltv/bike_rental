5bc9af3e156e64f2b1af115de58b61debdc633a5
Danil Treskov <treskovdanilv@gmail.com>
2024-08-07 17:11:37 +0300

    Create CustomUser model & serializer.

---
+++ bike_rental/users/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from rest_framework.serializers import ModelSerializer
+
+
+User = get_user_model()
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
