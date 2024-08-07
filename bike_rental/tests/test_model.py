096b53d230d7e35046512a9a1ce37c7968223d6c
Danil Treskov <treskovdanilv@gmail.com>
2024-08-07 20:07:15 +0300

    Create tests for models.

---
+++ bike_rental/tests/test_model.py
@@ -0,0 +1,14 @@
+import pytest
+from bike_rental.rentals.models import Bike, Rental
+
+
+@pytest.mark.django_db
+def test_bike_creation():
+    bike = Bike.objects.create(name='Test bike')
+    assert bike.name == 'Test bike'
+
+@pytest.mark.django_db
+def test_rental_creation(user, bike):
+    rental = Rental.objects.create(user=user, bike=bike)
+    assert rental.user == user
+    assert rental.bike == bike
