2bd18196f0742f1a35530051da401743836b6ae1
Danil Treskov <treskovdanilv@gmail.com>
2024-08-07 16:26:02 +0300

    Create ViewSets for Bike & Rental.

--- bike_rental/rentals/views.py
+++ bike_rental/rentals/views.py
@@ -1,2 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Bike, Rental
+from .serializers import BikeSerializer, RentalSerializer
 
+
+class BikeViewSet(ModelViewSet):
+    queryset = Bike.objects.all()
+    serializer_class = BikeSerializer
+
+
+class RentalViewSet(ModelViewSet):
+    queryset = Rental.objects.all()
+    serializer_class = RentalSerializer
