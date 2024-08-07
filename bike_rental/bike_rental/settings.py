5a3484ec664004287e44cb37a5d881991edb28b8
Danil Treskov <treskovdanilv@gmail.com>
2024-08-07 17:42:29 +0300

    Create Celery app, add to project settings.

--- bike_rental/bike_rental/settings.py
+++ bike_rental/bike_rental/settings.py
@@ -69,6 +69,9 @@ REST_FRAMEWORK = {
     ),
 }
 
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
