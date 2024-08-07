d8cbfe77ece0dc8cb27672b9dac8347ee9408506
Danil Treskov <treskovdanilv@gmail.com>
2024-08-07 16:56:34 +0300

    Register DRF simpleJWT in installed apps, add DRF config.

--- bike_rental/bike_rental/settings.py
+++ bike_rental/bike_rental/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'rentals',
 ]
 
@@ -55,6 +56,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 AUTH_PASSWORD_VALIDATORS = [
